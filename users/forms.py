e25f5a9e78e845dac3bccdfe7a99bccdd54b2b90
Ian Farai Madhara <iannyfarai@gmail.com>
2026-09-15 00:04:22 -0400

    added form validation logic on registration

---
+++ users/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+
+class UserRegisterForm(UserCreationForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        if User.objects.filter(email__iexact=email).exists():
+            raise forms.ValidationError('An account with this email already exists.')
+        return email
--- users/views.py
+++ users/views.py
@@ -1,5 +1,22 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import login
+from django.shortcuts import redirect, render
+
+from .forms import UserRegisterForm
 
 
 def register(request):
-    return render(request, 'users/register.html')
+    if request.user.is_authenticated:
+        return redirect('blog-home')
+
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, f'Welcome to Rozvi, {user.username}! Your account has been created.')
+            return redirect('blog-home')
+    else:
+        form = UserRegisterForm()
+
+    return render(request, 'users/register.html', {'form': form})
