c83f200308c2a105f604f8732f3e27bf09ffbd14
Ian Farai Madhara <iannyfarai@gmail.com>
2026-09-19 22:11:52 -0400

    added post creation

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='blog-home'),
     path('about/', views.about, name='blog-about'),
+    path('posts/new/', views.post_create, name='blog-post-create'),
     path('posts/<int:pk>/clap/', views.clap_post, name='blog-post-clap'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,18 +1,25 @@
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, redirect, render
 from django.views.decorators.http import require_POST
 
+from .forms import PostForm
 from .models import Post
+
+
+def _distinct_topics():
+    return (
+        Post.objects.order_by('topic')
+        .values_list('topic', flat=True)
+        .distinct()
+    )
 
 
 def home(request):
     posts = Post.objects.select_related('author', 'author__profile')
     staff_picks = posts.filter(featured=True)[:3]
-    topics = (
-        Post.objects.order_by('topic')
-        .values_list('topic', flat=True)
-        .distinct()
-    )
+    topics = _distinct_topics()
     context = {
         'posts': posts,
         'staff_picks': staff_picks,
@@ -23,6 +30,22 @@ def home(request):
 
 def about(request):
     return render(request, 'blog/about.html')
+
+
+@login_required
+def post_create(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            messages.success(request, 'Your story has been published.')
+            return redirect('blog-home')
+    else:
+        form = PostForm()
+
+    return render(request, 'blog/post_form.html', {'form': form, 'topics': _distinct_topics()})
 
 
 @require_POST
