4ce8d28adbd34c9695671ee5ecf263ef666d4e0b
Ian Farai Madhara <iannyfarai@gmail.com>
2026-09-20 23:23:26 -0400

    added post updating logic

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.home, name='blog-home'),
     path('about/', views.about, name='blog-about'),
     path('posts/new/', views.post_create, name='blog-post-create'),
+    path('posts/<int:pk>/edit/', views.post_update, name='blog-post-edit'),
     path('posts/<int:pk>/clap/', views.clap_post, name='blog-post-clap'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.core.exceptions import PermissionDenied
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404, redirect, render
 from django.views.decorators.http import require_POST
@@ -46,6 +47,24 @@ def post_create(request):
         form = PostForm()
 
     return render(request, 'blog/post_form.html', {'form': form, 'topics': _distinct_topics()})
+
+
+@login_required
+def post_update(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if post.author != request.user:
+        raise PermissionDenied('You can only edit your own stories.')
+
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your story has been updated.')
+            return redirect('blog-home')
+    else:
+        form = PostForm(instance=post)
+
+    return render(request, 'blog/post_form.html', {'form': form, 'topics': _distinct_topics(), 'post': post})
 
 
 @require_POST
