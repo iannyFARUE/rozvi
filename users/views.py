e303ad26cae8e746aff9db6a8c7b98d0a6d78c20
Ian Farai Madhara <iannyfarai@gmail.com>
2026-09-22 22:57:20 -0400

    added pagination on users own posts

--- users/views.py
+++ users/views.py
@@ -1,9 +1,11 @@
 from django.contrib import messages
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 from django.shortcuts import redirect, render
 
 from blog.models import Post
+from blog.views import POSTS_PER_PAGE
 
 from .forms import ProfileUpdateForm, UserRegisterForm, UserUpdateForm
 
@@ -38,11 +40,18 @@ def profile(request):
         u_form = UserUpdateForm(instance=request.user)
         p_form = ProfileUpdateForm(instance=request.user.profile)
 
-    posts = Post.objects.filter(author=request.user).select_related('author', 'author__profile')
+    posts_qs = Post.objects.filter(author=request.user).select_related('author', 'author__profile')
+    post_count = posts_qs.count()
+
+    paginator = Paginator(posts_qs, POSTS_PER_PAGE)
+    posts = paginator.get_page(request.GET.get('page'))
+    page_range = paginator.get_elided_page_range(posts.number, on_each_side=3, on_ends=1)
+
     context = {
         'u_form': u_form,
         'p_form': p_form,
         'posts': posts,
-        'post_count': posts.count(),
+        'page_range': page_range,
+        'post_count': post_count,
     }
     return render(request, 'users/profile.html', context)
